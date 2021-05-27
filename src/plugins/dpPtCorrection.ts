00fc56d3fcbe0e3c418b1511e9031dfa2334ece8
Joao Correia <jca.correia@gmail.com>
2021-05-28 01:17:09 +0200

    refactor(plugin): Reduce complexity

--- src/plugins/dpPtCorrection.ts
+++ src/plugins/dpPtCorrection.ts
@@ -1,33 +1,43 @@
 import { ConvertFunction } from '~/types'
 
-export const dpPtCorrection =
-    (pixelRatio: number, fontScale: number): ConvertFunction =>
-    (value: any) => {
-        // Groups                      Size (number)           Selector (type)
-        //                             1                       2
-        const validResponsiveRegex = /^(\-?\d+(?:\.\d{1,3})?)@(dp|pt)$/
-        const PX_PT_RATIO = 1.328
+let definedPixelRatio = 1
+let definedFontScale = 1
 
-        let result = value
-        if (!validResponsiveRegex.test(value)) {
-            return value
-        }
+export const getDpPtCorrection = (
+    pixelRatio: number,
+    fontScale: number,
+): ConvertFunction => {
+    definedPixelRatio = pixelRatio
+    definedFontScale = fontScale
+    return dpPtCorrection
+}
 
-        const regexExecResult = validResponsiveRegex.exec(value)
+const dpPtCorrection = (value: any) => {
+    // Groups                      Size (number)           Selector (type)
+    //                             1                       2
+    const validResponsiveRegex = /^(\-?\d+(?:\.\d{1,3})?)@(dp|pt)$/
+    const PX_PT_RATIO = 1.328
 
-        if (regexExecResult) {
-            const size = parseFloat(regexExecResult[1])
-            const selector = regexExecResult[2]
-
-            switch (selector) {
-                case 'dp':
-                    result = size * pixelRatio
-                    break
-                case 'pt':
-                    result = size * PX_PT_RATIO * fontScale
-                    break
-            }
-        }
-
-        return result
+    let result = value
+    if (!validResponsiveRegex.test(value)) {
+        return value
     }
+
+    const regexExecResult = validResponsiveRegex.exec(value)
+
+    if (regexExecResult) {
+        const size = parseFloat(regexExecResult[1])
+        const selector = regexExecResult[2]
+
+        switch (selector) {
+            case 'dp':
+                result = size * definedPixelRatio
+                break
+            case 'pt':
+                result = size * PX_PT_RATIO * definedFontScale
+                break
+        }
+    }
+
+    return result
+}
