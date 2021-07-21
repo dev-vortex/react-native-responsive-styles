3bb6a464238ca04b9557f190c71ac4f92552e5c0
Joao Correia <jca.correia@gmail.com>
2021-07-21 08:26:03 +0100

    fix: Rename and correct paths in test files

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import responsiveStyles, { ConvertPluginFunction } from '../src/index'
+import responsiveStyles, { ConvertPluginFunction } from '~/index'
 
 describe('[main]', () => {
     describe('import', () => {
